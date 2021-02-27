dedd388439a067ba2d2b4ba5dbf83d82240cb434
tebanban <estebancalf@gmail.com>
2021-02-27 15:33:58 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function mazo() {
+    const suits = ["hearts", "spades", "clubs", "diamonds"];
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10 ",
+      "J",
+      "Q",
+      "K"
+    ];
+    const cards = [];
+
+    for (let s = 0; s < suits.length; s++) {
+      for (let n = 0; n < suits.length; n++) {
+        const suit = suits[s];
+        const number = numbers[n];
+        cards.push({ number, suit });
+      }
+    }
+    this.console.log(cards);
+  }
+  return mazo;
 };
